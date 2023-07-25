c7e621e98b027aa79340258285e46265c927603d
jonny <jonnyshao@163.com>
2023-07-25 10:41:55 +0800

    add field

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,5 +1,14 @@
 import { PublicEntity } from 'src/shared/PublicEntity';
-import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
+
+export enum CURRENCY {
+  RMB = 'RMB',
+  USD = 'USD',
+}
+
+export enum TAXES {
+  VAT = '增值税',
+}
 
 @Entity({ name: 'customers', orderBy: { created_at: 'DESC' } })
 export class Customer extends PublicEntity {
@@ -7,4 +16,29 @@ export class Customer extends PublicEntity {
   name: string;
   @Column({ type: 'int', width: 50 })
   groud_id: number;
+  @Column({ type: 'char', length: 255, comment: '送货地址' })
+  delivery_address: string;
+  @Column({ type: 'char', length: 255, comment: '发票地址' })
+  invoice_address: string;
+  @Column({ type: 'varchar', length: 60, comment: '联系人', nullable: true })
+  contact: string;
+  @Column({ type: 'varchar', length: 60, comment: '联系电话', nullable: true })
+  mobile: string;
+  @Column({
+    type: 'enum',
+    enum: CURRENCY,
+    comment: '货币',
+    default: CURRENCY.RMB,
+  })
+  currency: CURRENCY;
+  @Column({
+    type: 'decimal',
+    width: 50,
+    precision: 2,
+    comment: '税率',
+    default: 0.13,
+  })
+  tax_rate: number;
+  @Column({ type: 'enum', enum: TAXES, comment: '税种', default: TAXES.VAT })
+  taxes: TAXES;
 }
