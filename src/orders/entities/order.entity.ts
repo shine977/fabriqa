830e0c71422bf0544434e559f8005af7471e4b24
jonny <jonnyshao@163.com>
2023-07-25 10:41:34 +0800

    add field

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -2,17 +2,24 @@ import { PublicEntity } from 'src/shared/PublicEntity';
 import { Column, Entity } from 'typeorm';
 @Entity({ name: 'orders', orderBy: { created_at: 'DESC' } })
 export class Order extends PublicEntity {
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, comment: '订单号' })
   no: string;
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, comment: '任务书号' })
   task_no: string;
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, comment: '单位', default: 'pcs' })
   unit: string;
 
-  @Column({ type: 'int', width: 50 })
+  @Column({ type: 'int', width: 50, comment: '数量' })
   quantity: number;
-  @Column({ type: 'decimal', width: 50, precision: 2 })
+  @Column({ type: 'decimal', width: 50, precision: 2, comment: '单价' })
   unit_price: number;
-  @Column({ type: 'decimal', width: 50, precision: 2 })
+  @Column({ type: 'decimal', width: 50, precision: 2, comment: '金额' })
   amount: number;
+
+  @Column({ type: 'datetime', length: 255, comment: '采购日期' })
+  purchase_date: number;
+  @Column({ type: 'varchar', length: 255, comment: '付款条件' })
+  payment_clause: string;
+  @Column({ type: 'datetime', length: 255, comment: '交期' })
+  delivery: string;
 }
