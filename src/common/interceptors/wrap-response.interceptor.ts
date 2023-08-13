b8496332da80f57929a8c7800e7464e57baa905f
jonny <jonnyshao@163.com>
2023-08-13 15:21:19 +0800

    add tenantInterceptor

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -1,13 +1,12 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
-import { UnifyResponse, UnifySigleResponse } from '../utils/unifyResponse';
+import { UnifyResponse } from '../utils/unifyResponse';
 
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<UnifyResponse>): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        console.log(data);
         const response = data || ({ code: 0, message: 'Succesfully' } as UnifyResponse);
         if (data) {
           if (!data.code) {
@@ -17,22 +16,6 @@ export class WrapResponseInterceptor implements NestInterceptor {
             response.message = 'Successfully';
           }
         }
-
-        // if (Array.isArray(data)) {
-        //   response.items = data;
-        // } else if (
-        //   typeof data == 'string' ||
-        //   typeof data == 'boolean' ||
-        //   typeof data == 'number'
-        // ) {
-        //   response.item = data;
-        // } else {
-        //   if (data.message) {
-        //     response = { ...response, ...data };
-        //   } else {
-        //     response.item = data;
-        //   }
-        // }
 
         return response;
       }),
